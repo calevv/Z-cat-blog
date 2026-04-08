17e3752353acbf274dd7ba36aff31605c8f9c6d2
calevv <ika818488@gmail.com>
2026-04-08 20:50:00 +0900

    feat(admin/login):aside ui틀 형성

--- src/app/admin/login/layout.tsx
+++ src/app/admin/login/layout.tsx
@@ -2,8 +2,27 @@ import { ReactNode } from "react";
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
-    <div>
-      <div>로그인 레이아웃 수정합시다</div>
+    <div className="flex min-h-screen w-full">
+      <aside className="bg-foreground w-[480px] flex-col justify-between p-12 lg:flex">
+        <div className="text-background">
+          <h1 className="text-lg font-bold">Z-cat.</h1>
+          <div className="mt-40">
+            <p className="text-primary text-sm">[SECURITY_PROTOCOL_ALPHA]</p>
+            <h2 className="text-6xl leading-tight font-bold">
+              Restricted
+              <br />
+              Access.
+            </h2>
+            <p>
+              "Humans continue to treat security as a suggestion. I've flagged
+              12 entries for excessive sentimentality."
+            </p>
+          </div>
+          <div className="text-muted-foreground font-mono text-xs">
+            SERIAL_NO: ZC-00192-B ADMIN ONLY
+          </div>
+        </div>
+      </aside>
       {children}
     </div>
   );
