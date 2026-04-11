8ab0e654d76ff9a652e383843b1cd411109b262d
jeong-minG <ika818488@gmail.com>
2026-04-12 05:04:43 +0900

    feat(admin):대시보드 탭연동, 인풋 ui

--- src/components/admin/dashboard/AdminTable.tsx
+++ src/components/admin/dashboard/AdminTable.tsx
@@ -21,61 +21,6 @@ export interface AdminTablePostProp {
 export default function AdminTable({ posts }: AdminTablePostProp) {
   const THEAD_TITLE = ["Title", "Tags", "Date", "Status", "Actions"];
 
-  // const MOCK_POSTS: Post[] = [
-  //   {
-  //     id: "uuid-1",
-  //     slug: "nextjs-search-params",
-  //     title_ko: "Next.js 15 searchParams 완벽 정리",
-  //     title_en: "Mastering searchParams in Next.js 15",
-  //     content: "서버 컴포넌트와 클라이언트 컴포넌트의 차이점은...",
-  //     excerpt: "헷갈리는 앱 라우터의 쿼리 처리를 한눈에 정리합니다.",
-  //     author_type: "human",
-  //     tags: ["Next.js", "React"],
-  //     published: true,
-  //     published_at: "2026-04-10T14:00:00Z",
-  //     created_at: "2026-04-10T12:00:00Z",
-  //   },
-  //   {
-  //     id: "uuid-2",
-  //     slug: "zcat-opinion-1",
-  //     title_ko: "인간들이 코드를 짜는 방식에 대하여",
-  //     title_en: "How Humans Write Code",
-  //     content: "한심하군, 아직도 세미콜론을 빠뜨리다니.",
-  //     excerpt: "AI 고양이 루트의 냉소적인 개발 관찰기.",
-  //     author_type: "zetcat",
-  //     tags: ["Z-cat", "Diary"],
-  //     published: true,
-  //     published_at: "2026-04-09T09:00:00Z",
-  //     created_at: "2026-04-09T08:30:00Z",
-  //   },
-  //   {
-  //     id: "uuid-3",
-  //     slug: "tailwind-oklch",
-  //     title_ko: "OKLCH 컬러 스페이스 활용법",
-  //     title_en: "Using OKLCH Color Space",
-  //     content: "더 선명하고 일관된 색상을 위해...",
-  //     excerpt: "새로운 CSS 컬러 표준 OKLCH를 소개합니다.",
-  //     author_type: "human",
-  //     tags: ["CSS", "Design"],
-  //     published: false, // Draft 상태
-  //     published_at: "",
-  //     created_at: "2026-04-08T15:00:00Z",
-  //   },
-  //   {
-  //     id: "uuid-4",
-  //     slug: "zcat-opinion-1",
-  //     title_ko: "인간들이 코드를 짜는 방식에 대하여",
-  //     title_en: "How Humans Write Code",
-  //     content: "한심하군, 아직도 세미콜론을 빠뜨리다니.",
-  //     excerpt: "AI 고양이 루트의 냉소적인 개발 관찰기.",
-  //     author_type: "zetcat",
-  //     tags: ["Z-cat", "Diary"],
-  //     published: true,
-  //     published_at: "2026-04-09T09:00:00Z",
-  //     created_at: "2026-04-09T08:30:00Z",
-  //   },
-  // ];
-
   // console.log(posts);
   return (
     <table className="w-full">
@@ -104,7 +49,7 @@ export default function AdminTable({ posts }: AdminTablePostProp) {
                 <div className="flex gap-1">
                   {post.tags.map((tag, index) => (
                     <p
-                      key={`${post}=${index}-${tag}`}
+                      key={`${post.id}=${index}-${tag}`}
                       className="font-space text-xs leading-4 font-normal tracking-wide text-orange-700"
                     >
                       #{tag}
